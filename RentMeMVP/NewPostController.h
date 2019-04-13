824dcf36ed11712db615406408da28578aafd732
Mizuki Zenta <mizukisb@gmail.com>
2019-04-13 20:19:18 +1000

    add new post page and add some field

---
+++ RentMeMVP/NewPostController.h
@@ -0,0 +1,14 @@
+//
+//  NewPostController.h
+//  RentMeMVP
+//
+//  Created by Mizuki Zenta on 13/4/19.
+//  Copyright © 2019 Mizuki Zenta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPostController : UIViewController
+
+@end
+
