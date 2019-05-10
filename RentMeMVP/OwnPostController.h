a5fe36e0d341fd03df9f8993be9949e86c11f95a
Mizuki Zenta <mizukisb@gmail.com>
2019-05-10 12:52:06 +1000

    added menu related view controller and back btn for each views

---
+++ RentMeMVP/OwnPostController.h
@@ -0,0 +1,17 @@
+//
+//  OwnPostController.h
+//  RentMeMVP
+//
+//  Created by Mizuki Zenta on 10/5/19.
+//  Copyright © 2019 Mizuki Zenta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OwnPostController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
