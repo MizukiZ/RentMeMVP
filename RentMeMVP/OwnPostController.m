fd9f6c2622db5743af96a62399c09d141109af17
Mizuki Zenta <mizukisb@gmail.com>
2019-06-04 18:31:18 +1000

    add post items on own post page

--- RentMeMVP/OwnPostController.m
+++ RentMeMVP/OwnPostController.m
@@ -7,9 +7,13 @@
 //
 
 #import "OwnPostController.h"
+#import "PostDetailController.h"
+@import Firebase;
+
 
 @interface OwnPostController ()
-
+@property (strong, nonatomic) FIRDatabaseReference *ref;
+@property (strong, atomic) NSMutableArray *postObjectArray;
 @end
 
 @implementation OwnPostController
@@ -20,16 +24,89 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.table.estimatedRowHeight = 80;
+    self.table.rowHeight = UITableViewAutomaticDimension;
+    
+    self.ref = [[FIRDatabase database] reference];
+    self.postObjectArray = [NSMutableArray array];
+    
+    [[[[self.ref child:@"Post"]queryOrderedByChild:@"user_id"] queryEqualToValue:[FIRAuth auth].currentUser.uid] observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        
+        NSDictionary *dict = snapshot.value;
+        
+        NSArray *keys = [dict allKeys];
+        for (int i = 0; i < keys.count; i++)
+        {
+            id key = keys[i];
+            // get each attirbutes
+            NSDictionary *value = dict[key];
+            
+            // add posts to post object array
+            [self.postObjectArray addObject:value];
+        }
+        
+         NSLog(@"post array %@", self.postObjectArray);
+        // reload the table view with fetched data
+        [self.table reloadData];
+    }withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+    }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (BOOL)allowsHeaderViewsToFloat{
+    return NO;
 }
-*/
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.postObjectArray count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    float height = 80.0f;
+    return height;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"tableCell";
+    
+    
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if(cell==nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:3];
+    
+    dispatch_async(dispatch_get_global_queue(0,0), ^{
+        NSData * data = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: self.postObjectArray[indexPath.row][@"image"]]];
+        if ( data == nil )
+            return;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // WARNING: is the cell still using the same data by this point??
+            imageView.image = [UIImage imageWithData: data];
+        });
+    });
+    
+    // set title to a label
+    UILabel *titleLabel = (UILabel *)[cell viewWithTag:1];
+    titleLabel.text = self.postObjectArray[indexPath.row][@"title"];
+    
+    
+    // set cost to a label
+    id cost = self.postObjectArray[indexPath.row][@"cost"];
+    UILabel *costLabel = (UILabel *)[cell viewWithTag:2];
+    NSString *costS = [NSString stringWithFormat:@"%@", cost];
+    NSString *dollar = @"$/night";
+    NSString *costSentence = [costS stringByAppendingString:dollar];
+    costLabel.text = costSentence;
+    
+    return cell;
+}
 
 @end
