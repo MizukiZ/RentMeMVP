d6264f2f52f502b05d19916acabc6321ae7be384
Mizuki Zenta <mizukisb@gmail.com>
2019-06-07 11:44:46 +1000

    add image edit ability for user

--- RentMeMVP/AccountViewController.m
+++ RentMeMVP/AccountViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AccountViewController.h"
+#import <SpinKit/RTSpinKitView.h>
 @import Firebase;
 @import GooglePlaces;
 
@@ -23,62 +24,194 @@
 
 @implementation AccountViewController{
      GMSAutocompleteFilter *_filter;
+    Boolean noCamera;
+    Boolean isEditMode;
 }
 - (IBAction)backBtn:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+// user image click event
+-(void)tapDetected{
+    
+    if(isEditMode){
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    
+    // show Dialog to choose between take a photo or pick up image from device
+    UIAlertController * alert = [UIAlertController
+                                 alertControllerWithTitle:@"Image upload"
+                                 message:@"Please choose a mothod for uploading your image."
+                                 preferredStyle:UIAlertControllerStyleAlert];
+    //Add Buttons
+    UIAlertAction* libraryButton = [UIAlertAction
+                                    actionWithTitle:@"Photo library"
+                                    style:UIAlertActionStyleDefault
+                                    handler:^(UIAlertAction * action) {
+                                        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                                        [self presentViewController:picker animated:YES completion:NULL];
+                                    }];
+    
+    UIAlertAction* takePhotoButton = [UIAlertAction
+                                      actionWithTitle:@"Take a photo"
+                                      style:UIAlertActionStyleDefault
+                                      handler:^(UIAlertAction * action) {
+                                          picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+                                          [self presentViewController:picker animated:YES completion:NULL];
+                                      }];
+    
+    UIAlertAction* cancelButton = [UIAlertAction
+                                   actionWithTitle:@"Cancel"
+                                   style:UIAlertActionStyleDefault
+                                   handler:^(UIAlertAction * action) {
+                                       // do nothing
+                                   }];
+    
+    //Add your buttons to alert controller
+    [alert addAction:libraryButton];
+    if(!noCamera){
+        // if there is a camera on the device, add this option
+        [alert addAction:takePhotoButton];
+    }
+    [alert addAction:cancelButton];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+        
+    }
+}
+
+// after user takes a image or choose an image, this callback is triggered
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.userImage.image = chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+
+// when user cancel the image picker view, this callback is triggered
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
 - (IBAction)updateBtn:(id)sender {
  //update button clicked
+    
+    // Get a reference to the storage service using the default Firebase App
+    FIRStorage *storage = [FIRStorage storage];
+    // Create a storage reference from our storage service
+    FIRStorageReference *storageRef = [storage reference];
     
     NSString *userName = self.userName.text;
     NSString *bio = self.userBio.text;
     
-    NSDictionary *updateUser = @{@"bio": bio, @"userName": userName};
+    // Generate a data from the image selected
+    NSData *imageData = UIImageJPEGRepresentation(self.userImage.image, 0.8);
     
-    [[[_ref child:@"Users"] child:[FIRAuth auth].currentUser.uid] updateChildValues:updateUser withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
-        if (error) {
-            UIAlertView *toast = [[UIAlertView alloc] initWithTitle:nil
-                                                            message:@"Sorry something went wrong!"
-                                                           delegate:nil
-                                                  cancelButtonTitle:nil
-                                                  otherButtonTitles:nil, nil];
-            [toast show];
-            
-            int duration = 1; // duration in seconds
-            
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, duration * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-                [toast dismissWithClickedButtonIndex:0 animated:YES];
-            });
-        } else {
-            
-            UIAlertView *toast = [[UIAlertView alloc] initWithTitle:nil
-                                                            message:@"Updated successfully!"
-                                                           delegate:nil
-                                                  cancelButtonTitle:nil
-                                                  otherButtonTitles:nil, nil];
-            [toast show];
-            
-            int duration = 1; // duration in seconds
-            
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, duration * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-                [toast dismissWithClickedButtonIndex:0 animated:YES];
-            });
-        }
-    }];
+    // set loading icon
+    RTSpinKitView *spinner = [[RTSpinKitView alloc] initWithStyle:RTSpinKitViewStyleWave];
+    spinner.spinnerSize = 60;
+    spinner.center = CGPointMake(self.view.frame.size.width  / 2,
+                                 self.view.frame.size.height / 2);
+    [self.view addSubview:spinner];
     
+    // directory ref
+    NSString *dir = @"UserImages/";
+    // unique id for the image
+    NSString *uuid = [[NSUUID UUID] UUIDString];
+    FIRStorageReference *userImageRef = [storageRef child:[dir stringByAppendingString:uuid]];
+    // Create the file metadata
+    FIRStorageMetadata *imageMetadata = [[FIRStorageMetadata alloc] init];
+    imageMetadata.contentType = @"image/jpeg";
+    
+    
+    // Upload the file to the path "images/rivers.jpg"
+    FIRStorageUploadTask *uploadTask = [userImageRef putData:imageData
+                                                    metadata:imageMetadata
+                                                  completion:^(FIRStorageMetadata *metadata,
+                                                               NSError *error) {
+                                                      if (error != nil) {
+                                                          // Uh-oh, an error occurred!
+                                                      } else {
+                                                          // Metadata contains file metadata such as size, content-type, and download URL.
+                                                          int size = metadata.size;
+                                                          // You can also access to download URL after upload.
+                                                          [userImageRef downloadURLWithCompletion:^(NSURL * _Nullable URL, NSError * _Nullable error) {
+                                                              if (error != nil) {
+                                                                  // Uh-oh, an error occurred!
+                                                              } else {
+                                                                  // get downloadURL
+                                                                  NSString *downloadURL = [URL absoluteString];
+                                                                  
+                                                                  // location update is diabled due to the google api pricing change
+                                                                  NSDictionary *updateUser = @{@"bio": bio, @"userName": userName, @"image": downloadURL};
+                                                               
+                                                                  // dissmiss the loading icon
+                                                                  [spinner stopAnimating];
+                                                                  
+                                                                  // update value to firebase database
+                                                                  [[[self->_ref child:@"Users"] child:[FIRAuth auth].currentUser.uid] updateChildValues:updateUser withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+                                                                      if (error) {
+                                                                          UIAlertView *toast = [[UIAlertView alloc] initWithTitle:nil
+                                                                                                                          message:@"Sorry something went wrong!"
+                                                                                                                         delegate:nil
+                                                                                                                cancelButtonTitle:nil
+                                                                                                                otherButtonTitles:nil, nil];
+                                                                          [toast show];
+                                                                          
+                                                                          int duration = 1; // duration in seconds
+                                                                          
+                                                                          dispatch_after(dispatch_time(DISPATCH_TIME_NOW, duration * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                                                                              [toast dismissWithClickedButtonIndex:0 animated:YES];
+                                                                          });
+                                                                          
+                                                                          // dismiss itself view
+                                                                          [self dismissViewControllerAnimated:YES completion:nil];
+                                                                      } else {
+                                                                          
+                                                                          UIAlertView *toast = [[UIAlertView alloc] initWithTitle:nil
+                                                                                                                          message:@"Updated successfully!"
+                                                                                                                         delegate:nil
+                                                                                                                cancelButtonTitle:nil
+                                                                                                                otherButtonTitles:nil, nil];
+                                                                          [toast show];
+                                                                          
+                                                                          int duration = 1; // duration in seconds
+                                                                          
+                                                                          dispatch_after(dispatch_time(DISPATCH_TIME_NOW, duration * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                                                                              [toast dismissWithClickedButtonIndex:0 animated:YES];
+                                                                          });
+                                                                          
+                                                                          // dismiss itself view
+                                                                          [self dismissViewControllerAnimated:YES completion:nil];
+                                                                      }
+                                                                  }];
+                                                                  
+                                                              }
+                                                          }];
+                                                      }
+                                                  }];
     
 }
 
 - (IBAction)editModeSwitch:(UISwitch *)sender {
     if (sender.on) {
+        isEditMode = true;
        [self editModeOn];
     } else {
+        isEditMode = false;
        [self editModeOff];
     }
 }
 
 - (void)editModeOn{
+    [self.updateButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     self.userName.enabled = YES;
     self.userLocation.enabled = YES;
     self.userBio.editable = YES;
@@ -86,6 +219,7 @@
 }
 
 - (void)editModeOff{
+    [self.updateButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
     self.userName.enabled = NO;
     self.userLocation.enabled = NO;
     self.userBio.editable = NO;
@@ -157,6 +291,25 @@ didFailAutocompleteWithError:(NSError *)error {
     
     // disable text input fields
     [self editModeOff];
+    
+    // add click event
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected)];
+    singleTap.numberOfTapsRequired = 1;
+    [self.userImage setUserInteractionEnabled:YES];
+    [self.userImage addGestureRecognizer:singleTap];
+    
+    noCamera = false;
+    // when a device does not have a camera show warning
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        noCamera = true;
+        UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"No camera is detected!"
+                                                              message:@"Device has no camera"
+                                                             delegate:nil
+                                                    cancelButtonTitle:@"OK"
+                                                    otherButtonTitles: nil];
+        
+        [myAlertView show];
+    }
     
      self.ref = [[FIRDatabase database] reference];
     
